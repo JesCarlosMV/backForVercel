2a9dc6fadc7ee6972d63fdfde2bf78874219b4e4
JesCarlosMV <amgtk@hotmail.com>
2024-04-22 11:20:05 +0200

    removed res.send

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ import express from 'express';
 const app = express();
 
 app.listen(process.env.PORT || 3000, (req, res) => {
-  res.send('Hello World');
   console.log(`Server running on port ${process.env.PORT || 3000}`);
 });

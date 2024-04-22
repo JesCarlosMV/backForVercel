de70cf26c05dde2f842392847b37a459e257d5ee
JesCarlosMV <amgtk@hotmail.com>
2024-04-22 12:12:36 +0200

    app.get with send hello world

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ const app = express();
 app.listen(process.env.PORT || 3000, (req, res) => {
   console.log(`Server running on port ${process.env.PORT || 3000}`);
 });
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});

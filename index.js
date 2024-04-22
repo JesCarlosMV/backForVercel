15f651a299c7b54b3f8ce7d75d70bfd9ac89afc5
JesCarlosMV <amgtk@hotmail.com>
2024-04-22 12:16:05 +0200

    fwerg

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.listen(process.env.PORT || 3000, (req, res) => {
   console.log(`Server running on port ${process.env.PORT || 3000}`);
 });
 
-app.get('/', (req, res) => {
+app.get('https://back-for-vercel.vercel.app/', (req, res) => {
   res.send('Hello World!');
 });

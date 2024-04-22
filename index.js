3ada7e7e943d01d50ef4c6b35f11186cde8b4266
JesCarlosMV <amgtk@hotmail.com>
2024-04-22 11:16:59 +0200

    res.send

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.listen(process.env.PORT || 3000, () =>
-  console.log(`Server running on port ${process.env.PORT || 3000}`)
-);
+app.listen(process.env.PORT || 3000, (req, res) => {
+  res.send('Hello World');
+  console.log(`Server running on port ${process.env.PORT || 3000}`);
+});
